088f99dc9c2fc9b86d029d978423a0c2f8955b46
Enol Fernandez <enol.fernandez@egi.eu>
2018-07-30 15:50:03 +0100

    Now with right version

--- build.sh
+++ build.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 
-# Builder of rpm 
-# Run it as 
+# Builder of rpm
+# Run it as
 # docker run -it -v $PWD:/nagios-plugin-notebooks centos:6 /nagios-plugin-notebooks/build.sh
 
 yum install -y epel-release
 yum install -y rpm-build epel-rpm-macros python-pbr git
 
 cd nagios-plugin-notebooks/
-python setup.py sdist
-mkdir -p /root/rpmbuild/SOURCES
-mv dist/nagios_plugin_notebooks-*.tar.gz /root/rpmbuild/SOURCES/
 
-rpmbuild -ba nagios-plugin-notebooks.spec 
+VERSION=$(python setup.py --version)
+
+python setup.py sdist
+mkdir -p /root/rpmbuild/SOURCES \
+         /root/rpmbuild/SPECS
+
+sed "s/^Version:.*/Version: $VERSION/" nagios-plugin-notebooks.spec > /root/rpmbuild/SPECS/nagios-plugin-notebooks.spec
+
+mv dist/nagios_plugin_notebooks-$VERSION.tar.gz /root/rpmbuild/SOURCES/
+
+rpmbuild -ba  /root/rpmbuild/SPECS/nagios-plugin-notebooks.spec
 mv /root/rpmbuild/RPMS/noarch/* .
