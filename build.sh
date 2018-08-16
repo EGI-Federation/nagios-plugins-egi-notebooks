3089a42886d99f79065b7a21ff8f53b6ed567477
Enol Fernandez <enol.fernandez@egi.eu>
2018-08-16 16:34:21 +0100

    use the Makefile

--- build.sh
+++ build.sh
@@ -9,15 +9,6 @@ yum install -y rpm-build epel-rpm-macros python-pbr git
 
 cd nagios-plugin-notebooks/
 
-VERSION=$(python setup.py --version)
+make rpm
 
-python setup.py sdist
-mkdir -p /root/rpmbuild/SOURCES \
-         /root/rpmbuild/SPECS
-
-sed "s/^Version:.*/Version: $VERSION/" nagios-plugin-notebooks.spec > /root/rpmbuild/SPECS/nagios-plugin-notebooks.spec
-
-mv dist/nagios_plugin_notebooks-$VERSION.tar.gz /root/rpmbuild/SOURCES/
-
-rpmbuild -ba  /root/rpmbuild/SPECS/nagios-plugin-notebooks.spec
 mv /root/rpmbuild/RPMS/noarch/* .
