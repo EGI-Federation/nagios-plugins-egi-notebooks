e4c26b3230d8572348603f1d618053a5ddd2970d
Sebastian Luna-Valero <sebastian.luna.valero@gmail.com>
2024-06-19 16:27:09 +0200

    python black

--- nagios_plugins_egi_notebooks/status.py
+++ nagios_plugins_egi_notebooks/status.py
@@ -7,52 +7,58 @@ from six.moves.urllib.parse import urljoin
 import requests
 from requests.exceptions import ConnectionError, HTTPError
 
+
 def status2code(status):
     code_map = {
-        'OK': 0,
-        'WARNING': 1,
-        'CRITICAL': 2,
+        "OK": 0,
+        "WARNING": 1,
+        "CRITICAL": 2,
     }
     return code_map.get(status.upper(), 2)
 
 
 def get_notebook_status(status_url, timeout=10):
-    logging.debug('Querying %s for status', status_url)
+    logging.debug("Querying %s for status", status_url)
     try:
         r = requests.get(status_url, timeout=timeout)
         r.raise_for_status()
-        logging.debug('Full message: %s' % r.text)
+        logging.debug("Full message: %s" % r.text)
         logging.info("%s: %s", r.reason, r.status_code)
         return status2code(r.reason)
     except (ConnectionError, HTTPError) as e:
         logging.info("CRITICAL: Unable to get status, %s", e)
-        return status2code('CRITICAL')
+        return status2code("CRITICAL")
 
 
 def main():
     parser = argparse.ArgumentParser(
-        description='Nagios plugin for EGI Notebooks',
+        description="Nagios plugin for EGI Notebooks",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-        fromfile_prefix_chars='@',
+        fromfile_prefix_chars="@",
         conflict_handler="resolve",
     )
 
-    parser.add_argument('-t', '--timeout', default=10, type=int,
-                        help='Timeout in seconds of the probe')
+    parser.add_argument(
+        "-t", "--timeout", default=10, type=int, help="Timeout in seconds of the probe"
+    )
 
-    parser.add_argument('--url', help='URL of the the EGI notebooks endpoint')
+    parser.add_argument("--url", help="URL of the the EGI notebooks endpoint")
 
-    parser.add_argument('--status-path', default='hub/metrics',
-                        help=('Path in the endpoint for the monitoring '
-                              'service'))
+    parser.add_argument(
+        "--status-path",
+        default="hub/metrics",
+        help=("Path in the endpoint for the monitoring " "service"),
+    )
 
-    parser.add_argument('-v', '--verbose', default=False, action='store_true',
-                        help='Be verbose')
+    parser.add_argument(
+        "-v", "--verbose", default=False, action="store_true", help="Be verbose"
+    )
 
-    parser.add_argument('-H', '--host', help='Host to be checked')
+    parser.add_argument("-H", "--host", help="Host to be checked")
 
-    parser.add_argument('-p', '--port', default=443, type=int,
-                        help='Port to be checked')
+    parser.add_argument(
+        "-p", "--port", default=443, type=int, help="Port to be checked"
+    )
 
     opts = parser.parse_args()
     log_level = logging.INFO
@@ -63,9 +69,9 @@ def main():
     # URL takes precedence over -H and -p
     if not opts.url:
         if not opts.host:
-            logging.error('Mising url or host to check')
-            sys.exit(status2code('CRITICAL'))
-        status_url = 'https://%s:%d/' % (opts.host, opts.port)
+            logging.error("Mising url or host to check")
+            sys.exit(status2code("CRITICAL"))
+        status_url = "https://%s:%d/" % (opts.host, opts.port)
     else:
         status_url = opts.url
 
